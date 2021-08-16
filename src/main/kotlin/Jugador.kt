f3312c066071264cf463c9fa34a1a193614b4256
carlosbellmont <carlos.bellmont@gmail.com>
2021-08-16 13:59:17 +0200

    Guardando ficheros

--- src/main/kotlin/Jugador.kt
+++ src/main/kotlin/Jugador.kt
@@ -1,3 +1,5 @@
+import java.io.File
+
 class Jugador(var nombre : String = "",
               var partidasJugadas : Int = 0,
               var tiempoJugado : Int = 0,
@@ -10,5 +12,26 @@ class Jugador(var nombre : String = "",
 
     fun getDetalles(): String {
         return "El jugador $nombre lleva $partidasJugadas partidas jugadas en $tiempoJugado horas jugadas, con un resultado de $kills bajas por $deaths muertes. Por tanto el K/D es de ${getKD()}"
+    }
+
+    fun guardarJugador(){
+        val archivo = File("Jugadores/$nombre.txt")
+        archivo.writeText("${partidasJugadas}\n")
+        archivo.appendText("${tiempoJugado}\n")
+        archivo.appendText("${kills}\n")
+        archivo.appendText("${deaths}\n")
+    }
+
+    fun cargarJugador(){
+        try {
+            val archivo = File("Jugadores/$nombre.txt")
+            val lineas = archivo.readLines()
+            partidasJugadas = lineas[0].toInt()
+            tiempoJugado = lineas[1].toInt()
+            kills = lineas[2].toInt()
+            deaths = lineas[3].toInt()
+        } catch (exception : Exception) {
+            println("Error la leer los datos.")
+        }
     }
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,4 +3,13 @@ fun main() {
     val jugador1 = Jugador("Alfredo", 15, 21, 24, 20)
 
     println(jugador1.getDetalles())
+    jugador1.guardarJugador()
+
+    val jugador2 = Jugador("Godofredo", 1, 2, 3, 4)
+    jugador2.cargarJugador()
+    jugador2.kills++
+    jugador2.kills++
+    jugador2.tiempoJugado++
+    println(jugador2.getDetalles())
+    jugador2.guardarJugador()
 }
